eb350fb6dfc0ebe9acc8dac68a3830e205d09076
yashc1607 <yashc1607@gmail.com>
2023-11-03 21:22:45 +0530

    create signup api route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import {signup} from '../controllers/auth.controller.js';
+const authRouter = express.Router();
+authRouter.post("/signup",signup);
+export default authRouter;
