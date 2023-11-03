c3af04864c56f34d8210e18e92df989dfdc1bcd7
yashc1607 <yashc1607@gmail.com>
2023-11-03 18:22:45 +0530

    api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import {test} from '../controllers/user.controller.js';
+const userRouter = express.Router();
+userRouter.get('/test',test);
+export default userRouter;
